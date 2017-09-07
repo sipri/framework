72e5a60a39ca18ac5ed7919cd52e1fb6961287d0
chanbong.park <siprikorea@gmail.com>
2017-09-07 09:37:34 +0900

    Modify [python/snippet/check_codesign.py]

--- python/snippet/check_codesign.py
+++ python/snippet/check_codesign.py
@@ -2,10 +2,10 @@ def VerifyCodeSign(file):
     import os
     import subprocess
     with open(os.devnull, 'w') as FNULL:
-		import colorama
-		colorama.init()
-		if subprocess.call(["..\SignTool\signtool", "verify", "/pa", "{0}".format(file)], stdout=FNULL, stderr=FNULL):
-			print colorama.Fore.LIGHTRED_EX + "[  FAILED  ] {0}".format(file)
-		else:
-			print colorama.Fore.LIGHTGREEN_EX + "[  PASSED  ] {0}".format(file)
-		colorama.Style.RESET_ALL
+        import colorama
+        colorama.init()
+        if subprocess.call(["..\SignTool\signtool", "verify", "/pa", "{0}".format(file)], stdout=FNULL, stderr=FNULL):
+            print(colorama.Fore.LIGHTRED_EX + "[  FAILED  ] {0}".format(file))
+        else:
+            print(colorama.Fore.LIGHTGREEN_EX + "[  PASSED  ] {0}".format(file))
+        print(colorama.Style.RESET_ALL)
